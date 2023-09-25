79044985490067972fff180b4c04430fdff5e301
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-09-25 20:36:26 +1300

    adds tests for the user table

--- database/db.py
+++ database/db.py
@@ -22,4 +22,10 @@ sqlite_handler.establish_db_connection()
 # print(sqlite_handler.get_humidity_reading_at_time('2023-09-19 05:01:11'))
 # print(sqlite_handler.get_time_reading_at_humidity(13))
 
+# Test - User Table
+# sqlite_handler.create_user_table()
+# sqlite_handler.insert_into_user_table('Hello world', 'Hello user')
+# sqlite_handler.get_user_readings()
+# print(sqlite_handler.get_chatbot_response_from_user_input('Hello someone'))
+
 sqlite_handler.terminate_db_connection()
