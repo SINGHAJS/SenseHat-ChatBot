6f4e693ebc71722f4594321e2ea0802211045740
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-09-25 20:05:45 +1300

    adds tests for SQLite functions

--- database/db.py
+++ database/db.py
@@ -4,17 +4,22 @@ sqlite_handler = SQLiteDatabaseHandler()
 sqlite_handler.establish_db_connection()
 
 # Test - Temperature Table
-sqlite_handler.create_temp_table()
-sqlite_handler.insert_into_temp_table(20.0)
-sqlite_handler.insert_into_temp_table(40.0)
-sqlite_handler.insert_into_temp_table(10.0)
-sqlite_handler.get_temperature_readings()
+# sqlite_handler.create_temp_table()
+# sqlite_handler.insert_into_temp_table(20.0)
+# sqlite_handler.insert_into_temp_table(40.0)
+# sqlite_handler.insert_into_temp_table(101.0)
+# sqlite_handler.get_temperature_readings()
+# print(sqlite_handler.get_temperature_reading_at_time('2023-09-25 07:00:07'))
+# print(sqlite_handler.get_time_reading_at_temperature(90))
 
 # Test - Humidity Table
-sqlite_handler.create_humidity_table()
-sqlite_handler.insert_into_humidity_table(11.0)
-sqlite_handler.insert_into_humidity_table(12.0)
-sqlite_handler.insert_into_humidity_table(13.0)
-sqlite_handler.get_humidity_readings()
+# sqlite_handler.create_humidity_table()
+# sqlite_handler.insert_into_humidity_table(11.0)
+# sqlite_handler.insert_into_humidity_table(12.0)
+# sqlite_handler.insert_into_humidity_table(13.0)
+# sqlite_handler.get_humidity_readings()
+
+# print(sqlite_handler.get_humidity_reading_at_time('2023-09-19 05:01:11'))
+# print(sqlite_handler.get_time_reading_at_humidity(13))
 
 sqlite_handler.terminate_db_connection()
