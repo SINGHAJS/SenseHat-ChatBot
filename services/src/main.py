11e3f4b7f911993296f03a1c61d3dbaecbba3999
oshH7 <oshhettiarachchi@gmail.com>
2023-10-09 17:44:38 +1300

    running services from top level directory as module working

--- services/src/main.py
+++ services/src/main.py
@@ -1,25 +1,25 @@
 import os
 import openai
 from dotenv import load_dotenv
-from chat_completion import ChatCompletion
-from speech_to_text import SpeechToText
-from ...database.local_database.SQLiteDatabaseHandler import SQLiteDatabaseHandler
+from services.src.chat_completion import ChatCompletion
+from services.src.speech_to_text import SpeechToText
+from database.local_database import SQLiteDatabaseHandler
 
 
 class ChatBot:
     
     def __init__(self):
-        self.audio_directory = "../../embedded/assets/audio_files/current_user_prompt"
+        self.audio_directory = "/home/s13/projects/sensehat_chatbot/embedded/assets/audio_files/current_user_prompt"
         self.wav_files = [f for f in os.listdir(self.audio_directory) if f.endswith(".wav")]
         self.current_temperature = 24
         self.current_humidity = 70
-        self.db = SQLiteDatabaseHandler()
+        self.db = SQLiteDatabaseHandler.SQLiteDatabaseHandler()
         self.db.establish_db_connection()
         self.db.create_user_table()
         load_dotenv()
         openai.api_key = os.environ["OPENAI_API_KEY"]
         self.chat_completion = ChatCompletion(openai, "gpt-3.5-turbo")
-        self.credentials_path = "../../credentials/chatbotproject-401307-d4e1b394c4f5.json"
+        self.credentials_path = "/home/s13/projects/sensehat_chatbot/credentials/chatbotproject-401307-d4e1b394c4f5.json"
         self.speech_to_text = SpeechToText(credentials_path=self.credentials_path)
         self.responses_file = "/home/s13/projects/sensehat_chatbot/embedded/assets/responses/responses.txt"
 
@@ -64,16 +64,7 @@ class ChatBot:
         print(f"Answer: {answer}")
         self.db.insert_into_user_table(result, answer)
         self.write_response_to_file(answer)
-        
-
-        
-        
-        # keyword -> 2 
-                        # string cuurent temp in room is + self.current_temp" (string ,string)
-        # keywords < 2
-                        # open ai to get reponse
-        
-
+    
 
 if __name__ == "__main__":
     bot = ChatBot()
